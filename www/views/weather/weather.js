45ffed0175699c674b7aa88379513a9dff68c320
Jeremy Wilken <gnomeontherun@users.noreply.github.com>
2016-05-22 08:46:06 -0500

    Update weather.js
    fix an issue where open weather map requires a key now

--- www/views/weather/weather.js
+++ www/views/weather/weather.js
@@ -4,7 +4,8 @@ angular.module('App').controller('WeatherCtrl', function ($scope, $http, $ionicL
     template: '<span class="icon spin ion-loading-d"></span> Loading forecast'
   });
 
-  $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=Key%20West,FL&cnt=7&units=imperial').success(function (data) {
+  // You need to get a key from Open Weather Map and put the key in API_KEY_HERE
+  $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=Key%20West,FL&cnt=7&units=imperial&APPID=API_KEY_HERE').success(function (data) {
     $ionicLoading.hide();
     $scope.weather = data;
   });
